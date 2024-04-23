1ea7152dd94e0ce8126866d12c2bd3e72f4907f9
JayArcher9 <143174572+JayArcher9@users.noreply.github.com>
2024-04-23 15:56:55 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,61 @@
+const cors = require('cors')
+
+require('dotenv').config()
+
+
+const express = require('express')
+const mongoose = require('mongoose')
+const clubRouter = require('./routes/clubRoutes')
+const eventRouter = require('./routes/eventRoutes')
+
+
+
+
+// express
+const app = express();
+
+
+
+app.use(cors());
+
+
+// middleware
+app.use(express.json());
+
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+});
+
+
+// routes
+app.use('/club', require('./routes/clubRoutes'))
+app.use('/event', require('./routes/eventRoutes'))
+
+
+const allowedOrigins = ['http://localhost:3000'];
+
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
+
+
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URL)
+    .then((result) => {
+        console.log("Connected to db")
+        //listen
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
 
